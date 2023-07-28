36f670ab00026860c413a7c334d4de19d7124f79
hostmatic <42303895+hostmatic@users.noreply.github.com>
2023-07-28 19:08:57 +0200

    Update Install.sh
    Update script to support more of script like execution instead of notes

--- Install.sh
+++ Install.sh
@@ -1,71 +1,75 @@
+ #     # #######  #####  ####### #     #    #    ####### ###  #####  
+ #     # #     # #     #    #    ##   ##   # #      #     #  #     # 
+ #     # #     # #          #    # # # #  #   #     #     #  #       
+ ####### #     #  #####     #    #  #  # #     #    #     #  #       
+ #     # #     #       #    #    #     # #######    #     #  #       
+ #     # #     # #     #    #    #     # #     #    #     #  #     # 
+ #     # #######  #####     #    #     # #     #    #    ###  #####  
+                                                                     
+  #####  #     #  #####  ####### ####### #     #  #####              
+ #     #  #   #  #     #    #    #       ##   ## #     #             
+ #         # #   #          #    #       # # # # #                   
+  #####     #     #####     #    #####   #  #  #  #####              
+       #    #          #    #    #       #     #       #             
+ #     #    #    #     #    #    #       #     # #     #             
+  #####     #     #####     #    ####### #     #  #####              
+                                                                     
 # Copyright 2023 Hostmatic Systems
 # [IMPORTANT] 
-# Tested on 32 bit Raspian Buster on Raspberry Pi 4
-# If using Steam Link, the OS MUST be Buster, no later OS supported as of 2023.
-# Supports hardware decoding and HEVC (H.265)
+# Tested on 64 bit Raspian Bullseye on Raspberry Pi 4 (8GB)
+# Supports hardware decoding using HEVC (H.265)
+# Update ["NAME_OF_PC"] & ["NAME_OF_APP"]
 
 #Install updates and apps
 sudo apt update
 sudo apt upgrade
-sudo reboot
 
-#Install Etherwake, Moonlight and CEC Utils
+#Install Moonlight, Etherwake, CEC Utils and Speedtest
 #sudo apt install steamlink
 curl -1sLf 'https://dl.cloudsmith.io/public/moonlight-game-streaming/moonlight-qt/setup.deb.sh' | distro=raspbian codename=buster sudo -E bash
-sudo apt install moonlight-qt
-sudo apt install etherwake
-sudo apt install cec-utils
+sudo apt install -y moonlight-qt
+sudo apt install -y etherwake
+sudo apt install -y cec-utils
+sudo apt install -y speedtest-cli
 
 #Scan for TV using CEC - Use to power on TV during Boot
 echo 'scan' | cec-client -s -d 1
 
+#Perform speed test, and adjust accordingly - This step is important to ensure there is no stuttering.
+speedtest-cli
+
 #Install Argon software for case
 curl https://download.argon40.com/argon1.sh | bash
-Sudo reboot
-argonone-config
 
-#Configure Xbox controller & Check audio
-#Now would be a good time to configure controller, using the Raspbian Desktop Bluetooth interface, before proceding to headless mode
-#Open a web browser, and check that the controller is detected (https://gamepad-tester.com/) and that audio works (Youtube.com)
-
-#Enable HEVC (H.265) support
-#Add this line to /boot/config.txt
-dtoverlay=rpivid-v4l2
-
-#Raspi-Config settings
-sudo raspi-config
-#1 System options 
-#   ^
-#   |->S2 Audio -> Select Mai PCM (HDMI)
-#   |->S5 Boot / Autologin -> Console Autologin
-#   |->S6 Network at boot -> Yes
-#   |->S7 Splash Screen -> Would you like to show splash screen at boot? = No
-
-#4 Performance
-#   |->P2 GPU Memory -> set to 256 MB
-sudo reboot
-
-#Run SteamLink manually just one time, to compile packages
-#steamlink
+#Enable HEVC (H.265) support and increase video memory
+sudo su
+echo "gpu_mem=256" >> /boot/config.txt
+echo "dtoverlay=rpivid-v4l2" >> /boot/config.txt
+#Replace display driver when using Bullseye
+sed -i 's/dtoverlay=vc4-kms-v3d/dtoverlay=vc4-fkms-v3d/g' /boot/config.txt
+exit
 
 #Boot auto start
-sudo nano /etc/rc.local
-#IN RC.LOCAL
-#Custom settings
-#
-#Perform WOL (Only works with Ethernet)
-sudo etherwake 00:00:00:00:00:00 &
-#Turn on TV - Input correct address from scan
-echo 'on 0.0.0.0' | cec-client -s -d 1 &
-#Starts our custom boot animation - If TV is off, this won't have time to be shown
-omxplayer /home/pi/steamdeckintro.mp4 &
-#Starts moonlight in the user context to load user configs and start Steam (renamed in Sunshine)
-#sudo -H -u pi moonlight-qt &
-sudo -H -u pi moonlight-qt stream 192.168.1.42 Steam &
+sudo su
+echo '#!/bin/sh -e' > /etc/rc.local
+echo "#" >> /etc/rc.local
+echo "# rc.local" >> /etc/rc.local
+echo "#" >> /etc/rc.local
+echo "sudo etherwake 04:D9:F5:FA:31:20 &" >> /etc/rc.local
+echo "echo 'on 0.0.0.0' | cec-client -s -d 1 &" >> /etc/rc.local
+echo "sudo -H -u pi moonlight-qt stream ["NAME_OF_PC"] ["NAME_OF_APP"] &" >> /etc/rc.local
+echo "" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+exit
 
-exit 0
+#Final manual touches - Case config and Moonlight pair
+argonone-config
+moonlight-qt pair ["NAME_OF_PC"]
 
-#Perform network test, and adjust Moonlight bandwidth
-sudo apt install speedtest-cli
-speedtest-cli
-#Adjust accordingly in moonlight, this step is important to ensure there is no stuttering.
+#Lastly disable Desktop mode - You may need to set audio mode to HDMI after reboot
+sudo raspi-config
+#1 System Options
+#S5 Boot / Auto Login
+#B2 Console Autologin
+
+#Perform an reboot
